b256f5519cbbcce96c377c7b2c9a3234e5089a29
Murilo Gama <murilo.gcn@gmail.com>
2020-01-13 00:20:11 -0300

    Add coontacts loop

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="header-site">
+      <div class="content">
+        <div class="logo-site">
+          <img alt="Logo Instruct" src="./assets/instruct.svg">
+        </div>
+        <div class="filter">
+            <label for="filter"><b>Filtrar por email:</b></label>
+            <select name="" id="filter">
+              <option value="">Todos</option>
+            </select>
+        </div>
+      </div>
+    </header>
+    <section class="list-contacts">
+      <div class="content">
+        <ContactCard v-for="contact in contacts" v-bind:key= contact.id
+          v-bind:name= "contact.name"
+        ></ContactCard>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+import ContactCard from './components/ContactCard.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      loading: true,
+      contacts: [],
+      domains: null
+    }
+  },
+  methods: {
+    getContacts () {
+      axios
+      .get('http://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        /* eslint-disable no-console */
+        console.log(response.data);
+        /* eslint-enable no-console */
+        this.contacts = response.data
+      })
+      // .catch(error => {
+      //   console.log(error)
+      //   //this.errored = true
+      // })
+      .finally(() => this.loading = false)
+    }
+  },
+  created() {
+    this.getContacts();
+  },
   components: {
-    HelloWorld
+    ContactCard
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  background: #f2f2f2;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.header-site {
+  background: #23A5FA;
+  background: -moz-linear-gradient(45deg, #1e73aa 0%, #23a5fa 100%);
+  background: -webkit-linear-gradient(45deg, #1e73aa 0%, #23a5fa 100%);
+  background: linear-gradient(45deg, #1e73aa 0%, #23a5fa 100%);
+}
+.header-site .content {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 15px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
---
+++ src/components/ContactCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <address class="card">
+    <h1 class="card-header">{{ name }}</h1>
+  </address>
+</template>
+
+<script>
+export default {
+  name: 'ContactCard',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  background: #ffffff;
+  border: 1px solid #dddddd;
+  margin-bottom: 15px;
+  width: 25%;
+  box-shadow: 0 3px 10px 0 rgba(0,0,0,0.24);
+  border-radius: 10px;
+}
+</style>
